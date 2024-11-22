d285eee7929bd28e19849b0d2761386593be634f
crogers <cwrogiv@gmail.com>
2024-11-21 19:04:25 -0700

    starting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import MenuComponent from "./components/MenuComponent";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <MenuComponent/>
+        <div className="content">
+          {children}
+        </div>
       </body>
     </html>
   );
