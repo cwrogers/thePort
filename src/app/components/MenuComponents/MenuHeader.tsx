ce92bbfdc7359270b3bd780dbbd4657e556867fa
crogers <cwrogiv@gmail.com>
2024-11-26 21:05:30 -0800

    Add the image

--- src/app/components/MenuComponents/MenuHeader.tsx
+++ src/app/components/MenuComponents/MenuHeader.tsx
@@ -1,12 +1,15 @@
 import styles from '../componentStyles/components.module.css';
+import Image from "next/image";
 
 const MenuHeaderComponent = () => {
     return (
         <div className={styles.headerArea}>
-            <img
-                src="https://via.placeholder.com/150"
-                alt="Charles Rogers"
+            <Image
+                src={`/20240615_191036.png`}
+                alt={`Charles Rogers`}
                 className={styles.headerImage}
+                width={100}
+                height={100}
             />
             <h1 className={styles.headingTitle}>Charles Rogers</h1>
             <h4 className={styles.headingSubtitle}>Software Engineer</h4>
--- src/app/components/MenuComponents/MobileMenuComponent.tsx
+++ src/app/components/MenuComponents/MobileMenuComponent.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState, useEffect } from 'react';
+import Image from "next/image";
 
 const MobileMenuComponent = () => {
     const [shadow, setShadow] = useState(false);
@@ -12,12 +13,14 @@ const MobileMenuComponent = () => {
     return (
         <div className={`flex flex-row justify-between items-center p-4 text-whit mobile-menu-bar ${shadow && 'shadow'}`}>
             <div className="flex flex-row items-center">
-                <img
-                    src="https://via.placeholder.com/150"
-                    alt="Charles Rogers"
-                    className="rounded-full h-10 w-10"
-                />
-                <div className="ml-2">
+                <Image
+                    src={`/20240615_191036.png`}
+                    alt={''}
+                    width={50}
+                    height={50}
+                    className={`rounded-full`}
+                    />
+                <div className="ml-4">
                     <h1 className="text-lg font-semibold">Charles Rogers</h1>
                     <h4 className="text-sm">Software Engineer</h4>
                     <p className="text-xs">Phoenix, AZ</p>
