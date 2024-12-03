b51c656b8469007b2a6e35ee49a9ce77b3ed717f
crogers <cwrogiv@gmail.com>
2024-12-03 08:15:30 -0800

    Add date env var

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'standalone',
+    env: {
+        BUILD_DATE: new Date().toLocaleString('en-US', {month: 'short',day: 'numeric',year: 'numeric'})
+    }
 };
 
 export default nextConfig;
