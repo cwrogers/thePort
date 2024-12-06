2dc3eb6d475e60ebe0e55944fed09759c28508ff
crogers <cwrogiv@gmail.com>
2024-12-06 10:48:07 -0800

    try again

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,16 +3,22 @@
 import posthog from "posthog-js";
 import {PostHogProvider} from "posthog-js/react";
 import {ScriptProps} from "next/script";
+import {useEffect} from "react";
 
 
-if (typeof window !== "undefined") {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        person_profiles: "never"
-    });
-}
+
 
 export const ThePortPostHogProvider = ({ children } : ScriptProps) => {
+    useEffect(() => {
+        posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
+            api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+            person_profiles: "never",
+            capture_pageview: true,
+            capture_heatmaps: true,
+            capture_dead_clicks: true,
+            autocapture: true
+        });
+    }, []);
     return (
         <PostHogProvider client={posthog}>
             {children}
